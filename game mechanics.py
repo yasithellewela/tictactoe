277d0c9b40267482648448de8144e496ad4d567e
Yasith <yasth.ellewela02@gmail.com>
2020-04-16 08:30:22 -0400

    Added visual board function and game checking function (no actual functionality yet)

--- game mechanics.py
+++ game mechanics.py
@@ -1,1 +1,24 @@
-print("Welcome")
+board = [['0', '0', '0'],
+         ['0', '0', '0'],
+         ['0', '0', '0']]
+
+
+def printBoard(currentboard):
+    stop = False
+    counter = 0
+    while not stop:
+        print(currentboard[counter])
+        counter += 1
+        if counter == 3:
+            stop = True
+
+
+def gamecheck(nope):
+    return nope
+
+
+print("TicTacToe")
+end = False
+while not end:
+    printBoard(board)
+    end = True
